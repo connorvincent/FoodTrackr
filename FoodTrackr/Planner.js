3225f830843bca036a5fa7b9b85593bc8cd1d510
BTLundy97 <brandon.lundy@selu.edu>
2017-10-06 14:15:53 -0500

    Began working with CalendarList. Still a few unresolved issues, but they should be resolved soon

--- FoodTrackr/Planner.js
+++ FoodTrackr/Planner.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import {
     StyleSheet, Text, View, Image, Dimensions,
-    TouchableOpacity, AppRegistry, Alert, FlatList
+    TouchableOpacity, AppRegistry, Alert, ScrollView, Calendar, CalendarList, Agenda
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { Constants,} from 'expo';
-
+import {LocaleConfig} from 'react-native-calendars';
+LocaleConfig.locales['fr'] = {
+	monthNames: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+	monthNamesShort: ['Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'],
+	dayNames: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+	dayNamesShort: ['Sun.','Mon.','Tue.','Wed.','Thurs.','Fri.','Sat.']
+};
+ 
+LocaleConfig.defaultLocale = 'fr';
 var screenWidth = Dimensions.get('window').width;
 var screenHeight = Dimensions.get('window').height;
 
 export default class PlannerScreen extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {};
+		this.onDayPress = this.onDayPress.bind(this);
 	}
     static navigationOptions = {
         title: 'Planner',
@@ -24,21 +34,49 @@ export default class PlannerScreen extends React.Component {
 
 
     render() {
-		var menus = [
-			{key: "This is where the menus will appear when I can get them working. I am using the text as a placeholder so it doesn't throw an empty key error."}
-		];
+		
         const { navigate } = this.props.navigation;
         return (
-			
             <View style={styles.top}>
-                <View style={{ height: (screenHeight * 0.75) - Constants.statusBarHeight }}>
-					<FlatList
-				data = {menus}
-				renderItem = {
-				({item}) => <Text>{item.key}</Text>
-				}
-				>
-				</FlatList>
+                <View>
+					<div>
+					<Calendar
+						current = {'2017-10-01'}
+						minDate = {'2017-10-01'}
+						maxDate = {'2021-10-01'}
+						onDayPress ={(day) => {console.log('selected day', day)}}
+						monthFormat={'yyyy MM'}
+						onMonthChange ={ (month) => {console.log('month changed', month)}}
+						hideArrows = {true}
+						renderArrow = {(direction) => (<Arrow />)}
+						hideExtraDays = {true}
+						disableMonthChange = {true}
+						firstDay = {1}
+						/>
+					<Calendar
+						markedDates = {{}}
+					/>
+					<Calendar 
+						 style={{
+							borderWidth: 1,
+							borderColor: 'gray',
+							height: 350
+						}}
+						theme={{
+						 calendarBackground: '#ffffff',
+						 textSectionTitleColor: '#b6c1cd',
+						 selectedDayBackgroundColor: '#00adf5',
+						 selectedDayTextColor: '#ffffff',
+						 todayTextColor: '#00adf5',
+						 dayTextColor: '#2d4150',
+							textDisabledColor: '#d9e1e8',
+							dotColor: '#00adf5',
+							selectedDotColor: '#ffffff',
+							arrowColor: 'orange',
+							monthTextColor: 'blue'
+						}}
+						/>
+					</div>
                 </View>
                 <View style={styles.bMenu}>
                     <View style={styles.bMenu}>
