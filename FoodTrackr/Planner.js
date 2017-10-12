1e5a446426edbe4d71e43a3895b51eb1d7c56dd0
BTLundy97 <brandon.lundy@selu.edu>
2017-10-12 14:06:42 -0500

    Fixed undefined object issues in Planner

--- FoodTrackr/Planner.js
+++ FoodTrackr/Planner.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import {
     StyleSheet, Text, View, Image, Dimensions,
-    TouchableOpacity, AppRegistry, Alert, ScrollView, Calendar, CalendarList, Agenda
+    TouchableOpacity, AppRegistry, Alert, ScrollView
 } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars'
 import { StackNavigator } from 'react-navigation';
 import { Constants,} from 'expo';
-import {LocaleConfig} from 'react-native-calendars';
-LocaleConfig.locales['fr'] = {
-	monthNames: ['January','February','March','April','May','June','July','August','September','October','November','December'],
-	monthNamesShort: ['Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'],
-	dayNames: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-	dayNamesShort: ['Sun.','Mon.','Tue.','Wed.','Thurs.','Fri.','Sat.']
-};
- 
-LocaleConfig.defaultLocale = 'fr';
+
 var screenWidth = Dimensions.get('window').width;
 var screenHeight = Dimensions.get('window').height;
 
@@ -21,7 +14,7 @@ export default class PlannerScreen extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
-		this.onDayPress = this.onDayPress.bind(this);
+		
 	}
     static navigationOptions = {
         title: 'Planner',
@@ -38,46 +31,17 @@ export default class PlannerScreen extends React.Component {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.top}>
-                <View>
-					<div>
-					<Calendar
-						current = {'2017-10-01'}
-						minDate = {'2017-10-01'}
-						maxDate = {'2021-10-01'}
-						onDayPress ={(day) => {console.log('selected day', day)}}
-						monthFormat={'yyyy MM'}
-						onMonthChange ={ (month) => {console.log('month changed', month)}}
-						hideArrows = {true}
-						renderArrow = {(direction) => (<Arrow />)}
-						hideExtraDays = {true}
-						disableMonthChange = {true}
-						firstDay = {1}
-						/>
-					<Calendar
-						markedDates = {{}}
-					/>
-					<Calendar 
-						 style={{
-							borderWidth: 1,
-							borderColor: 'gray',
-							height: 350
-						}}
-						theme={{
-						 calendarBackground: '#ffffff',
-						 textSectionTitleColor: '#b6c1cd',
-						 selectedDayBackgroundColor: '#00adf5',
-						 selectedDayTextColor: '#ffffff',
-						 todayTextColor: '#00adf5',
-						 dayTextColor: '#2d4150',
-							textDisabledColor: '#d9e1e8',
-							dotColor: '#00adf5',
-							selectedDotColor: '#ffffff',
-							arrowColor: 'orange',
-							monthTextColor: 'blue'
-						}}
-						/>
-					</div>
+                <View style={{ height: (screenHeight * 0.75) - Constants.statusBarHeight }}>
                 </View>
+				<ScrollView style={styles.calendar}>
+					<Text style={styles.text}>Calendar with selectable date and arrows</Text>
+					<Calendar
+						 onDayPress={this.onDayPress}
+						 style={styles.calendar}
+						hideExtraDays
+						markedDates={{[this.state.selected]: {selected: true}}}
+					/>
+					</ScrollView>
                 <View style={styles.bMenu}>
                     <View style={styles.bMenu}>
                         <TouchableOpacity onPress={() => navigate('Inventory')}>
@@ -104,6 +68,11 @@ export default class PlannerScreen extends React.Component {
         );
     }
 }
+function onDayPress(day) {
+    this.setState({
+      selected: day.dateString
+    });
+  }
 
 const styles = StyleSheet.create({
 
@@ -135,5 +104,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#D3D3D3',
     },
+	calendar: {
+		borderTopWidth: 1,
+		paddingTop: 5,
+		borderBottomWidth: 1,
+		borderColor: '#eee',
+		height: 350
+	},
+	text: {
+		textAlign: 'center',
+		borderColor: '#bbb',
+		padding: 10,
+		backgroundColor: '#eee'
+	},
+	container: {
+		flex: 1,
+		backgroundColor: 'gray'
+	}
 
 });
